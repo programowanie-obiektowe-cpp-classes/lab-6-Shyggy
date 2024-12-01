9740adeb5cf4d4f1c66f01fe8a9f9260bcedba7e
Jędrzej <mr.myszer@gmail.com>
2024-12-01 16:00:11 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,13 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
-
+    std::for_each(people.begin(), people.end(), [](Human& a) { a.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](Human& a) -> char {
+        if (a.isMonster() == false) {
+            return 'y';
+        }
+        else
+            return 'n';
+        });
     return retval;
 }
